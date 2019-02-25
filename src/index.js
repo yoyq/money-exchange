6cc12f36c8a8cc891de0ac6a97900d82c517d3ed
dima <3277898@gmail.com>
2019-02-25 22:42:09 +0300

    money exchange task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let coinsType = {
+        H: 50,
+        Q: 25,
+        D: 10,
+        N: 5,
+        P: 1
+    };
+    let resObj = {};
+
+    if (currency <= 0) { return {}; }
+
+    if (currency > 10000) { return {error: "You are rich, my friend! We don't have so much coins for exchange"}; }
+
+    for (let p in coinsType) {
+        if (!coinsType.hasOwnProperty(p)) { continue; }
+
+        let v = parseInt(currency / coinsType[p]);
+        if (v) {
+            resObj[p] = v;
+            currency = currency % coinsType[p];
+        }
+    }
+
+    return resObj;
 }
